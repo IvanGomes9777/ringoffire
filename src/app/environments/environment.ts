f78d99eb56ec2f445e73796b2b7bb844b9957b9c
Ivan <ivanvilargomes@gmail.com>
2023-11-06 21:56:40 +0100

    first commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:false,
+
+    firebase:{
+        apiKey: "AIzaSyDxwsbh85Nf6S7b2m431BgSmU6FRZ2gnGM",
+        authDomain: "ring-of-fire-1a689.firebaseapp.com",
+        projectId: "ring-of-fire-1a689",
+        storageBucket: "ring-of-fire-1a689.appspot.com",
+        messagingSenderId: "1062848530894",
+        appId: "1:1062848530894:web:52e82269fd198df536439f"
+    }
+}; 
